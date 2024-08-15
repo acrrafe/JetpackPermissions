29225ec4f933a48f4c5bd6a84717ce982f119ce9
Aron Rafe <lnsapollo0@gmail.com>
2024-08-15 22:02:23 +0800

    One time Camera Permission

--- app/src/main/java/com/example/jetpackcomposepermissions/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepermissions/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.jetpackcomposepermissions
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.jetpackcomposepermissions.ui.theme.JetpackComposePermissionsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +26,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePermissionsTheme {
-                // A surface container using the 'background' color from the theme
+
+                val viewModel = viewModel<MainViewModel>()
+                val dialogQueue = viewModel.visiblePermissionDialogQueue
+
+                val cameraPermission = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { isGranted ->
+                        viewModel.onPermissionResult(
+                            permission =  Manifest.permission.CAMERA,
+                            isGranted = isGranted
+                        )
+
+                    }
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Button(onClick = {
+                            cameraPermission.launch(
+                                Manifest.permission.CAMERA
+                            )
+                        }) {
+                            Text(text = "Camera Permission")
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Multiple Permission")
+                        }
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposePermissionsTheme {
-        Greeting("Android")
     }
 }
